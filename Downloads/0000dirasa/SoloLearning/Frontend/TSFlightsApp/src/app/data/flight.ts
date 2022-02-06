63f3489990fa9afcb07b225c4841c3574c16b1f4
Abire Ghallabi <abireghallabii@gmail.com>
2022-02-06 01:22:25 +0100

    css

---
+++ Downloads/0000dirasa/SoloLearning/Frontend/TSFlightsApp/src/app/data/flight.ts
@@ -0,0 +1,6 @@
+export const data=[
+    {origin:"Miami",destination:'Chicago',flightNumber:345,depart:new Date('2020-02-25T23:18:21.932Z'),
+    arrive:new Date('2020-02-25T23:21:21.932Z'),nonstop:true},
+    {origin:"New York",destination:'Los Angeles',flightNumber:432,depart:new Date('2020-05-25T23:18:00.932Z'),
+    arrive:new Date('2020-05-25T23:23:21.932Z'),nonstop:false},
+    ];
